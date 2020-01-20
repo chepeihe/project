8450653df725b80550aa8e696c2fcbd3579d7a37
chepeihe <15718807527@163.com>
2020-01-20 13:52:21 +0800

    上传测试代码

---
+++ src/main/java/com/test/project/com/test/controller/TestClass.java
@@ -0,0 +1,24 @@
+package com.test.project.com.test.controller;
+
+/**
+ * @program: project
+ * @create: 2020-01-20 13:45
+ **/
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+    }
+
+}
---
+++ src/main/java/com/test/project/com/test/controller/TestDemo2.java
@@ -0,0 +1,26 @@
+package com.test.project.com.test.controller;
+
+/**
+ * @program: project
+ * @description: 测试
+ * @author: chepeihe
+ * @create: 2020-01-20 13:49
+ **/
+public class TestDemo2 {
+
+    public static void main(String[] args) {
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+        System.out.println("打印");
+    }
+
+}
